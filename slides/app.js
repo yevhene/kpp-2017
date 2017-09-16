9a0ea76fe65acbcfa25f542cdff627a525db53e9
Yevhen Shemet <yevhene@gmail.com>
2017-09-16 13:50:50 +0300

    Slides integrated

---
+++ slides/app.js
@@ -0,0 +1,27 @@
+const glob = require('glob');
+const path = require('path');
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname);
+
+app.use(expressLayouts);
+app.set('layout', path.join(__dirname, 'layouts/layout'));
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', (req, res) => res.render('lectures'));
+
+glob.sync(path.join(__dirname, 'lectures/*/')).forEach(dir => {
+  const lecture = path.basename(dir);
+  app.get(`/${lecture}`, (req, res) =>
+    res.render(path.join('lectures', lecture))
+  );
+  app.use(express.static(path.join(__dirname, 'lectures', lecture, 'assets')));
+});
+
+module.exports = app;
